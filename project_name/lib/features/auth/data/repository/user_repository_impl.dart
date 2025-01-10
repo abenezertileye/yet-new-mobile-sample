1423e30238ace2e42fe697bd1e791a2fd7568ec7
abene <abenezertileye2003@gmail.com>
2025-01-10 13:51:40 +0300

    fix

---
+++ project_name/lib/features/auth/data/repository/user_repository_impl.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:project_name/features/auth/data/data_source/remote_data_source.dart';
+import 'package:project_name/features/auth/domain/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final UserDataSource dataSource;
+
+  UserRepositoryImpl(this.dataSource);
+
+  @override
+  Future<String> login(String email, String password) {
+    return dataSource.login(email, password);
+  }
+}
