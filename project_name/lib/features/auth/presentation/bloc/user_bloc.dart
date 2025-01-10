3374de470bb605399428e4656a204e29654eaa5f
abene <abenezertileye2003@gmail.com>
2025-01-10 13:51:47 +0300

    fix

---
+++ project_name/lib/features/auth/presentation/bloc/user_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_name/features/auth/domain/user_repository.dart';
+import 'user_event.dart';
+import 'user_state.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  final UserRepository repository;
+
+  UserBloc(this.repository) : super(UserInitial()) {
+    on<UserLoginEvent>((event, emit) async {
+      emit(UserLoading());
+      try {
+        final response = await repository.login(event.email, event.password);
+        emit(UserSuccess(response));
+      } catch (e) {
+        emit(UserFailure(e.toString()));
+      }
+    });
+  }
+}
