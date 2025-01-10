2baabc56d56fcbd82d011f6abd3557a877651165
abene <abenezertileye2003@gmail.com>
2025-01-10 13:51:35 +0300

    fix

---
+++ project_name/lib/features/auth/data/data_source/remote_data_source.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class UserDataSource {
+  final String baseUrl;
+  final storage = FlutterSecureStorage();
+
+  UserDataSource({required this.baseUrl});
+
+  Future<String> login(String email, String password) async {
+    final url = Uri.parse('$baseUrl/users/login');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({"email": email, "password": password}),
+    );
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      // Decode the JSON response
+      final responseData = jsonDecode(response.body);
+
+      // Store the 'USER_ID' as a string
+      await storage.write(
+          key: 'USER_ID', value: responseData['_id'].toString());
+
+      return response.body;
+    } else {
+      throw Exception('Failed to log in');
+    }
+  }
+}
