f3c7d46caff3ffc5d751c331ab3fbe5429d49b2f
abene <abenezertileye2003@gmail.com>
2025-02-04 16:06:16 +0300

    fix

--- project_name/lib/features/view_data/data/data_source/remote_data_source.dart
+++ project_name/lib/features/view_data/data/data_source/remote_data_source.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:project_name/features/view_data/data/model/schedule.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ScheduleDataSource {
   final String baseUrl;
+  final storage = FlutterSecureStorage();
 
   ScheduleDataSource({required this.baseUrl});
 
@@ -14,6 +16,22 @@ class ScheduleDataSource {
       final data = json.decode(response.body);
       final List<dynamic> schedulesJson = data['data'];
       return schedulesJson.map((json) => Schedule.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load schedules');
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchUserData() async {
+    final userId = await storage.read(key: 'USER_ID');
+
+    final response =
+        await http.get(Uri.parse('$baseUrl/users/get-user-info/${userId}'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      final Map<String, dynamic> userData = data['data'];
+      return userData;
     } else {
       throw Exception('Failed to load schedules');
     }
