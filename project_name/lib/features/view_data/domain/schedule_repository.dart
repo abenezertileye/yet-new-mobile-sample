b2828ca457096d6ae2f996c6d1e32485417f24e6
abene <abenezertileye2003@gmail.com>
2025-02-04 16:06:25 +0300

    fix

--- project_name/lib/features/view_data/domain/schedule_repository.dart
+++ project_name/lib/features/view_data/domain/schedule_repository.dart
@@ -2,4 +2,5 @@ import 'package:project_name/features/view_data/data/model/schedule.dart';
 
 abstract class ScheduleRepository {
   Future<List<Schedule>> getSchedules();
+  Future<Map<String, dynamic>> getUserData();
 }
