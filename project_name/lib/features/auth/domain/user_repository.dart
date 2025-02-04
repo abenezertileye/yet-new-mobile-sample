cc14d79883de9daea61d6b4e938386e65dc0644d
abene <abenezertileye2003@gmail.com>
2025-02-04 16:06:00 +0300

    fix

--- project_name/lib/features/auth/domain/user_repository.dart
+++ project_name/lib/features/auth/domain/user_repository.dart
@@ -1,4 +1,5 @@
-
 abstract class UserRepository {
   Future<String> login(String email, String password);
+  Future<String> signup(
+      String firstName, String lastName, String email, String password);
 }
