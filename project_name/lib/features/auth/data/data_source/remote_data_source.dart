19219b9ad51f0af2fa42cdcb6008278d1663e985
abene <abenezertileye2003@gmail.com>
2025-02-04 16:05:49 +0300

    fix

--- project_name/lib/features/auth/data/data_source/remote_data_source.dart
+++ project_name/lib/features/auth/data/data_source/remote_data_source.dart
@@ -16,7 +16,7 @@ class UserDataSource {
       body: jsonEncode({"email": email, "password": password}),
     );
 
-    print(response.body);
+    print('responseData: ${response.body}');
 
     if (response.statusCode == 200) {
       // Decode the JSON response
@@ -27,6 +27,36 @@ class UserDataSource {
           key: 'USER_ID', value: responseData['_id'].toString());
 
       return response.body;
+    } else {
+      throw Exception('Failed to log in');
+    }
+  }
+
+  Future<String> signup(
+      String firstName, String lastName, String email, String password) async {
+    final url = Uri.parse('$baseUrl/users/signup');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        "first_name": firstName,
+        "last_name": lastName,
+        "email": email,
+        "password": password
+      }),
+    );
+
+    print('responseData: ${response.body}');
+
+    if (response.statusCode == 200) {
+      // Decode the JSON response
+      final responseData = jsonDecode(response.body);
+
+      // Store the 'USER_ID' as a string
+      await storage.write(
+          key: 'USER_ID', value: responseData['_id'].toString());
+
+      return response.body;   
     } else {
       throw Exception('Failed to log in');
     }
